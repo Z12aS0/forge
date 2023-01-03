1cf3146961e56c374f0e1045925e8739f682ff8e
Z12 <80636711+yesyesyes69@users.noreply.github.com>
2023-01-03 22:38:21 +0200

    remove profile id requirement

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {ActivityIndicator, Text, View,StyleSheet, TextInput, Button, Image} from
 import * as Notifications from 'expo-notifications';
 import * as SecureStore from 'expo-secure-store';
 import forgeconvert from "./forgeconvert.json";
-var forgetime,forgeendtime1,forgeendtime2,forgeendtime3,forgeendtime4,forgeendtime5,forgeuntil1,forgeuntil2,forgeuntil3,forgeuntil4,forgeuntil5,forgetime1,forgetime2,forgetime3,forgetime4,forgetime5;
+var timeleft,forgetime,forgeendtime1,forgeendtime2,forgeendtime3,forgeendtime4,forgeendtime5,forgeuntil1,forgeuntil2,forgeuntil3,forgeuntil4,forgeuntil5,forgetime1,forgetime2,forgetime3,forgetime4,forgetime5,forgeid1,forgeid2,forgeid3,forgeid4,forgeid5;
 
 
   
@@ -39,41 +39,61 @@ export default class App extends React.Component {
   
   getdata = async () => {
     try {
-      var profileidsuccess = 'no';
       var uuidsuccss = 'no';
       var apikeysuccess = 'no';
-      const profileid = await SecureStore.getItemAsync('profileid');
       const uuid = await SecureStore.getItemAsync('uuid');
       const apikey = await SecureStore.getItemAsync('apikey');
-      if(profileid) {  profileidsuccess = 'yes'}
       if(uuid) { uuidsuccss = 'yes'}
       if(apikey) { apikeysuccess = 'yes'}
-      if(uuid && profileid && apikey)
+      if(uuid && apikey)
       {
-      fetch('https://api.hypixel.net/skyblock/profile?key=' + apikey + '&profile=' + profileid)
+      try{
+      var profiledata1 = await fetch('https://api.hypixel.net/skyblock/profiles?uuid=' + uuid + '&key=' + apikey )
       .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading:false,
-          profiledata: responseJson,
-          uuid: uuid,
-          pisc: profileidsuccess,
-          uisc: uuidsuccss,
-          apsc: apikeysuccess,
-        }, function(){
-        });
-        
-      })
-      .catch((error) =>{
-        alert('No internet connection or invalid api key/profile id');
-      });
-    }
-    else{
+      for(let i = 0; i < 69; i++) //get selected profile
+      {
+        if(profiledata1.profiles[i].selected == true)
+        {
+          var profiledata = profiledata1.profiles[i];
+          break;
+        }
+      }
+    } catch (a){
+      alert('No internet connection or invalid api key/uuid')
       this.setState({
         isLoading:false,
         profiledata: null,
         uuid: uuid,
-        pisc: profileidsuccess,
+        uisc: uuidsuccss,
+        apsc: apikeysuccess
+      });
+      return;
+    }
+     //const bazaardata = await fetch('https://api.hypixel.net/skyblock/bazaar').then((response) => response.json())
+
+
+
+
+
+
+      this.setState({
+        isLoading:false,
+        profiledata: profiledata,
+        uuid: uuid,
+        uisc: uuidsuccss,
+        apsc: apikeysuccess,
+      })
+
+
+
+      }
+      
+    
+    else{
+      this.setState({
+        isLoading:false,
+        profiledata: null,
+        uuid: null,
         uisc: uuidsuccss,
         apsc: apikeysuccess
       });
@@ -91,6 +111,8 @@ export default class App extends React.Component {
       console.log(e);
     }
   };
+
+
 
   componentDidMount()
   {
@@ -110,64 +132,72 @@ export default class App extends React.Component {
     )
   }
   try{
-    var profiledata = this.state.profiledata.profile.members[this.state.uuid];
+    var profiledata = this.state.profiledata.members[this.state.uuid];
   }
   catch(a)
   {
-    this.savedata('apikey','')
-    this.savedata('profileid','')
-    this.savedata('uuid','')
+    //this.savedata('apikey','')
+    //this.savedata('profileid','')
+    //this.savedata('uuid','')
   }
  
   if(this.state.uuid && this.state.profiledata)
   {
     var forge = profiledata.forge.forge_processes.forge_1;
-    var timeleft;
   if(profiledata.mining_core.nodes.forge_time){
     var forgetime_ = profiledata.mining_core.nodes.forge_time;
-  if(profiledata.mining_core.nodes.forge_time < 20)
-  {
-    forgetime = (0.9 - forgetime_*0.005)
-  }
-  else
-  {
-    forgetime = 0.7
-  }
-}
-else
-{
-  forgetime = 1
-}
+    if(profiledata.mining_core.nodes.forge_time < 20)
+    {
+      forgetime = (0.9 - forgetime_*0.005)
+    }
+    else
+    {
+      forgetime = 0.7
+    }
+    }
+    else
+    {
+      forgetime = 1
+    }
+
+
+
 
 
 
 
 
 if(forge['1']){
+  forgeid1 = forge['1'].id.toLowerCase();
   forgetime1 = 3600000*forgetime*forgeconvert[forge['1'].id];
   forgeendtime1 = moment(new Date(forge['1'].startTime + forgetime1)).format('hh:mm A');
   forgeuntil1 =  moment(forge['1'].startTime + forgetime1).fromNow();
 }
 if(forge['2']){
+  forgeid2 = forge['2'].id.toLowerCase();
   forgetime2 = 3600000*forgetime*forgeconvert[forge['2'].id];
   forgeendtime2 = moment(new Date(forge['2'].startTime + forgetime2)).format('hh:mm A');
   forgeuntil2 =  moment(forge['2'].startTime + forgetime2).fromNow();
 }
 if(forge['3']){
+  forgeid3 = forge['3'].id.toLowerCase();
   forgetime3 = 3600000*forgetime*forgeconvert[forge['3'].id];
   forgeendtime3 = moment(new Date(forge['3'].startTime + forgetime3)).format('hh:mm A');
   forgeuntil3 =  moment(forge['3'].startTime + forgetime3).fromNow();
 }
 if(forge['4']){
+  forgeid4 = forge['4'].id.toLowerCase();
   forgetime4 = 3600000*forgetime*forgeconvert[forge['4'].id];
   forgeendtime4 = moment(new Date(forge['4'].startTime + forgetime4)).format('hh:mm A');
   forgeuntil4 =  moment(forge['4'].startTime + forgetime4).fromNow();
 }
 if(forge['5']){
+  forgeid5 = forge['5'].id.toLowerCase();
   forgetime5 = 3600000*forgetime*forgeconvert[forge['5'].id];
   forgeendtime5 = moment(new Date(forge['5'].startTime + forgetime5)).format('hh:mm A');
   forgeuntil5 =  moment(forge['5'].startTime + forgetime5).fromNow();
 }
+
 if(forge['1'])
 {
 if(forge['1'].startTime + forgetime1 - Date.now() > 0)
@@ -188,7 +218,6 @@ if(forge['1'].startTime + forgetime1 - Date.now() > 0)
 
     
  
-
   Notifications.scheduleNotificationAsync({
     content: {
     title: "Forge - ",
@@ -196,11 +225,9 @@ if(forge['1'].startTime + forgetime1 - Date.now() > 0)
     priority:'max',
     data: { data: 'collect forge' },
     },
-    trigger: { seconds: timeleft },
+    trigger: { seconds: 1 },
     });
-
   }
-
     
   return(
 <View style={{flex:1, flexDirection: 'column', backgroundColor:"#242323"}}>
@@ -208,22 +235,21 @@ if(forge['1'].startTime + forgetime1 - Date.now() > 0)
     
         
         
-        <Text style={styles.plate}>{forge['1'].id.toLowerCase()} ending at: {forgeendtime1} ({forgeuntil1}) </Text>
-        <Text style={styles.plate}>{forge['2'].id.toLowerCase()} ending at: {forgeendtime2} ({forgeuntil2}) </Text>
-        <Text style={styles.plate}>{forge['3'].id.toLowerCase()} ending at: {forgeendtime3} ({forgeuntil3}) </Text>
-        <Text style={styles.plate}>{forge['4'].id.toLowerCase()} ending at: {forgeendtime4} ({forgeuntil4}) </Text>
-        <Text style={styles.plate}>{forge['5'].id.toLowerCase()} ending at: {forgeendtime5} ({forgeuntil5}) </Text>
+        <Text style={styles.plate}>{forgeid1} ending at: {forgeendtime1} ({forgeuntil1}) </Text>
+        <Text style={styles.plate}>{forgeid2} ending at: {forgeendtime2} ({forgeuntil2}) </Text>
+        <Text style={styles.plate}>{forgeid3} ending at: {forgeendtime3} ({forgeuntil3}) </Text>
+        <Text style={styles.plate}>{forgeid4} ending at: {forgeendtime4} ({forgeuntil4}) </Text>
+        <Text style={styles.plate}>{forgeid5} ending at: {forgeendtime5} ({forgeuntil5}) </Text>
         <TextInput style={styles.textinput1} placeholder={"api key: " + this.state.apsc} onSubmitEditing={event =>
          {alert('api key saved: ' + event.nativeEvent.text )
-      this.savedata("apikey", event.nativeEvent.text)}}>
+      this.savedata("apikey", event.nativeEvent.text)
+      }}>
     </TextInput>
-    <TextInput style={styles.textinput1} placeholder={"profile id: " + this.state.pisc} onSubmitEditing={event =>
-         {alert('profile id saved: ' + event.nativeEvent.text )
-      this.savedata("profileid", event.nativeEvent.text)}}>
-    </TextInput>
-    <TextInput style={styles.textinput1} placeholder={"uuid: " + this.state.uisc} onSubmitEditing={event =>
-         {alert('uuid saved: ' + event.nativeEvent.text )
-      this.savedata("uuid", event.nativeEvent.text)}}>
+    <TextInput style={styles.textinput1} placeholder={"uuid: " + this.state.uuid} onSubmitEditing={event =>
+         {
+      alert('uuid saved: ' + event.nativeEvent.text )
+      this.savedata("uuid", event.nativeEvent.text)
+      }}>
     </TextInput>
     <Button
     title="Clear notifications"
@@ -233,6 +259,34 @@ if(forge['1'].startTime + forgetime1 - Date.now() > 0)
      alert('Notifications cleared')
     }}
     ></Button>
+    <View style={{marginTop:20}}>
+    <Button
+    title="Test notification"
+    style={{textAlign:'center',backgroundColor:'gray' }}
+    onPress={() => {
+      Notifications.scheduleNotificationAsync({
+        content: {
+        title: "Forge - ",
+        body: 'Forge is ready',
+        priority:'max',
+        data: { data: 'collect forge' },
+        },
+        trigger: { seconds: 1 },
+        });
+    }}
+    ></Button>
+    </View>
+    <View style={{marginTop:20}}>
+    <Button
+    title="Reload data(spamming => invalid api key)"
+    style={{textAlign:'center',backgroundColor:'gray' }}
+    onPress={() => {
+      this.getdata();
+      this.render();
+    }}
+    ></Button>
+    </View>
+    
     </View>
   );
 }  
@@ -282,7 +336,7 @@ textinput1:
 {
   marginLeft:48,
   marginRight:48,
-  fontSize: 25,
+  fontSize: 20,
    marginBottom:15,
    backgroundColor:"gray",
     color:'white',
