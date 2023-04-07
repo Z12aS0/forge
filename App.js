fc6896fe996c5032012b1c81c709a6ee1f7da7c8
Z12 <80636711+Z12aS0@users.noreply.github.com>
2023-04-07 21:00:44 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,32 +3,193 @@ import {ActivityIndicator, Text, View,StyleSheet, TextInput, Button, ToastAndroi
 import * as Notifications from 'expo-notifications';
 import * as SecureStore from 'expo-secure-store';
 import forgeconvert from "./forgeconvert.json";
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+const BACKGROUND_FETCH_TASK = 'Forge-Background-Task';
+
+const Stack = createNativeStackNavigator();
 
 var titletext,forgetime,forgeendtime1,forgeendtime2,forgeendtime3,forgeendtime4,forgeendtime5,forgeuntil1,forgeuntil2,forgeuntil3,forgeuntil4,forgeuntil5,forgetime1,forgetime2,forgetime3,forgetime4,forgetime5,forgeid1,forgeid2,forgeid3,forgeid4,forgeid5,forgeend1,forgeend2,forgeend3,forgeend4,forgeend5,midnightReset,heavypearldisplay,pearlsReset, heavypearldisplay1, godpotdisplay;
 var timeleft = 1;
 
-Notifications.setNotificationHandler({
-handleNotification: () => {
-return {
-shouldShowAlert: true
-}}
-})
 
-export default class App extends React.Component {
+
+
+
+async function notify(timeleft, text = "Forge is ready", noToast = 0){
+  await Notifications.scheduleNotificationAsync({
+    content: {
+    title: "Forge",
+    body: text,
+    priority:'max',
+    data: { data: 'collect forge' },
+    },trigger: { seconds: timeleft }});
+    if(!noToast){
+    setTimeout(() => {
+      ToastAndroid.show('Notification set', ToastAndroid.SHORT);
+    }, 5000)}}
+async function registertask(interval=8){
+
+    let a = await TaskManager.isTaskRegisteredAsync(BACKGROUND_FETCH_TASK);
+    if(!a){
+    BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+      minimumInterval: 60*60*interval, //8h maybe
+      stopOnTerminate: false, startOnBoot: true});
+    ToastAndroid.show('Toggled on(interval:' + interval + "h)", ToastAndroid.SHORT);
+  }else{
+    BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
+    ToastAndroid.show('Toggled off', ToastAndroid.SHORT);
+   }
+   
+}
+async function savedata(item,value){
+  try {await SecureStore.setItemAsync(item,value)} catch (e) {console.log(e)}}
+
+
+async function getapikey(apikey){
+  try {
+    var apikey1 = await fetch("https://api.hypixel.net/key?key=" + apikey).then((response) => response.json())
+    if(apikey1.success == true)
+    {savedata('apikey', apikey)
+    ToastAndroid.show('api key saved: ' + apikey,ToastAndroid.SHORT);}
+    else{ToastAndroid.show('Invalid api key', ToastAndroid.SHORT); 
+      if(apikey == "")
+      {savedata('apikey',"")}}
+  } catch (e) {ToastAndroid.show('Error, failed to check api key validity.', ToastAndroid.SHORT);}}
+
+
+async function getuuid(username){
+  try {
+    var uuid1 = await fetch("https://api.mojang.com/users/profiles/minecraft/" + username).then((response) => response.json())
+    if(uuid1 == "")
+    {ToastAndroid.show('Invalid username', ToastAndroid.SHORT); // not possible but whatever
+    }
+    else{
+    savedata('uuid', uuid1.id)
+    ToastAndroid.show('uuid saved: ' + uuid1.id,ToastAndroid.SHORT);}} catch (e) {
+      ToastAndroid.show('Invalid username', ToastAndroid.SHORT);}}
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer theme={{colors:{background:'#242323', backgroundColor:'#242323',card:'#242323',text:'#bcc3cf'}}}>
+     <Stack.Navigator>
+      <Stack.Screen name="Forge" component={Home}/>
+      <Stack.Screen name="Config" component={Config} />
+     </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+
+Notifications.setNotificationHandler({
+  handleNotification: () => {
+  return {
+  shouldShowAlert: true
+  }}
+  })
+
+TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+  var uuid = await SecureStore.getItemAsync('uuid');
+  var apikey = await SecureStore.getItemAsync('apikey');
+  try{
+    var profiledata1 = await fetch('https://api.hypixel.net/skyblock/profiles?uuid=' + uuid + '&key=' + apikey )
+    .then((response) => response.json())
+    if(profiledata1.success == true) //is something invalid?
+    {
+    for(let i = 0; i < 69; i++) //get selected profile
+    {
+      if(profiledata1.profiles[i].selected == true)
+      {
+        var profiledata = profiledata1.profiles[i].members[uuid];
+        break;
+      }
+    }
+  } else {
+    if(profiledata1.cause == "Malformed UUID")
+    { notify(1,"Failed to execute auto notification: invalid UUID",1)}
+
+    else if(profiledata1.cause == "Invalid API key")
+    {notify(1,"Failed to execute auto notification: Invalid API key",1)}}
+
+  } catch (a){
+    if(a == 'TypeError: Network request failed')
+
+    {notify(1,"Failed to execute auto notification: no internet connection.",1)}
+
+    else{notify(1,"Failed to execute auto notification: Error with api: " + a,1)}
+
+  }
+
+  if(!profiledata)return
+    var forge = profiledata.forge.forge_processes.forge_1;
+
+  if(profiledata.mining_core.nodes.forge_time){
+    var forgetime_ = profiledata.mining_core.nodes.forge_time;
+    if(profiledata.mining_core.nodes.forge_time < 20)
+      {forgetime = (0.9 - forgetime_*0.005)}else{forgetime = 0.7}}
+    else{forgetime = 1} //quickforge stuff
+
+if(forge['1']){
+  forgeid1 = forge['1'].id.toLowerCase();
+  forgetime1 = 3600000*forgetime*forgeconvert[forge['1'].id];
+  forgeend1 = forge['1'].startTime + forgetime1;
+  if(forgeend1 - Date.now() > 0){ 
+  timeleft = (forgeend1 - 180000 - Date.now())/1000; } else { timeleft = 1; }}
+  if(forge['2']){
+    forgeid2 = forge['2'].id.toLowerCase();
+    forgetime2 = 3600000*forgetime*forgeconvert[forge['2'].id];
+    forgeend2 = forge['2'].startTime + forgetime2;
+    if(forgeend2 - Date.now() > 0){ 
+    timeleft = (forgeend2 - 180000 - Date.now())/1000; } else { timeleft = 1; }}
   
+  if(forge['3']){
+    forgeid3 = forge['3'].id.toLowerCase();
+    forgetime3 = 3600000*forgetime*forgeconvert[forge['3'].id];
+    forgeend3 = forge['3'].startTime + forgetime3;
+    if(forgeend3 - Date.now() > 0){
+    timeleft = (forgeend3 - 180000 - Date.now())/1000; } else { timeleft = 1; }}
+  
+  if(forge['4']){
+    forgeid4 = forge['4'].id.toLowerCase();
+    forgetime4 = 3600000*forgetime*forgeconvert[forge['4'].id];
+    forgeend4 = forge['4'].startTime + forgetime4;
+    if(forgeend4 - Date.now() > 0){ 
+    timeleft = (forgeend4 - 180000 - Date.now())/1000; } else { timeleft = 1; }}
+  
+  if(forge['5']){
+    forgeid5 = forge['5'].id.toLowerCase();
+    forgetime5 = 3600000*forgetime*forgeconvert[forge['5'].id];
+    forgeend5 = forge['5'].startTime + forgetime5;
+    if(forgeend5 - Date.now() > 0){
+    timeleft = (forgeend5 - 180000 - Date.now())/1000; } else { timeleft = 1; }} 
+
+
+  notify(timeleft);
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
+
+
+
+
+
+
+class Home extends React.Component{
   state ={
     isLoading:true,
     uuid:'',
     profiledata:'',
-    pisc:'',
-    apsc:'',
-    uisc:''
+
   }
   getdata = async () => {
     try {
-      
-      var uuidsuccess = 'no';
-      var apikeysuccess = 'no';
       var uuid = await SecureStore.getItemAsync('uuid');
       var apikey = await SecureStore.getItemAsync('apikey');
       var i1 = await SecureStore.getItemAsync('i1');
@@ -41,9 +202,6 @@ export default class App extends React.Component {
       var e3 = await SecureStore.getItemAsync('e3');
       var e4 = await SecureStore.getItemAsync('e4');
       var e5 = await SecureStore.getItemAsync('e5');
-      
-      if(uuid) { uuidsuccess = 'Valid'}
-      if(apikey) { apikeysuccess = 'Valid'}
       if(uuid && apikey)
       {
       try{
@@ -72,35 +230,29 @@ export default class App extends React.Component {
       else{
         alert('Api request failed\nError: ' + a);
       }
-      
+      //something failed
       this.setState({
         isLoading:false,
         profiledata: "",
         uuid: uuid,
-        uisc: uuidsuccess,
-        apsc: apikeysuccess,
         cache: {i1:i1,i2:i2,i3:i3,i4:i4,i5:i5,e1:e1,e2:e2,e3:e3,e4:e4,e5:e5}
       });
       return;
     }
-     //const bazaardata = await fetch('https://api.hypixel.net/skyblock/bazaar').then((response) => response.json())
-    //prob never gonna use this
+      //success
       this.setState({
         isLoading:false,
         profiledata: profiledata,
         uuid: uuid,
-        uisc: uuidsuccess,
-        apsc: apikeysuccess,
         cache: {i1:i1,i2:i2,i3:i3,i4:i4,i5:i5,e1:e1,e2:e2,e3:e3,e4:e4,e5:e5}
       })
       }
     else{
+      //no uuid or(and) api key
       this.setState({
         isLoading:false,
         profiledata: "",
         uuid: "",
-        uisc: uuidsuccess,
-        apsc: apikeysuccess,
         cache: {i1:i1,i2:i2,i3:i3,i4:i4,i5:i5,e1:e1,e2:e2,e3:e3,e4:e4,e5:e5}
       });
     }
@@ -109,69 +261,7 @@ export default class App extends React.Component {
     }
   
   };
-
-  savedata = async (item,value) => {
-    try {
-      await SecureStore.setItemAsync(item,value);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  getuuid = async (username) => {
-    try {
-      var uuid1 = await fetch("https://api.mojang.com/users/profiles/minecraft/" + username)
-      .then((response) => response.json())
-      if(uuid1 == "")
-      {
-        ToastAndroid.show('Invalid username', ToastAndroid.SHORT); // not possible but whatever
-      }
-      else{
-      this.savedata('uuid', uuid1.id)
-      ToastAndroid.show('uuid saved: ' + uuid1.id,ToastAndroid.SHORT);
-      }
-    } catch (e) {
-      ToastAndroid.show('Invalid username', ToastAndroid.SHORT);
-    }
-  };
-
-  getapikey = async (apikey) => {
-    try {
-      var apikey1 = await fetch("https://api.hypixel.net/key?key=" + apikey)
-      .then((response) => response.json())
-      if(apikey1.success == true)
-      {
-        this.savedata('apikey', apikey)
-        ToastAndroid.show('api key saved: ' + apikey,ToastAndroid.SHORT);
-      }
-      else{
-        ToastAndroid.show('Invalid api key', ToastAndroid.SHORT); 
-        if(apikey == "")
-        {
-          this.savedata('apikey',"")
-        }
-      }
-    } catch (e) {
-      ToastAndroid.show('Error, failed to check api key validity.', ToastAndroid.SHORT);
-    }
-  };
-
-  notify = async (timeleft) => {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-      title: "Forge",
-      body: 'Forge is ready',
-      priority:'max',
-      data: { data: 'collect forge' },
-      },
-      trigger: { seconds: timeleft },
-      });
-      setTimeout(() => {
-        ToastAndroid.show('Notification set', ToastAndroid.SHORT);
-      }, 5000);
-      
-  }
-
+   
   componentDidMount()
   {
     this.getdata();
@@ -180,36 +270,23 @@ export default class App extends React.Component {
 
  render() 
  {
-  
   var moment = require('moment');
   if (this.state.isLoading) {
     return(
       <View style={{flex: 1, paddingTop: 40}}>
-        <Text style={{textAlign:"center"}}>Loading...</Text>
+        <Text style={{textAlign:"center",color:'#bcc3cf'}}>Loading...</Text>
       <ActivityIndicator/>
       </View>
     )
   }
-    
-  
-
- 
   if(this.state.uuid && this.state.profiledata)
   {
-    midnightReset = (Date.now() - 18000000) / 86400000 * 86400000 + 18000000;
-    pearlsReset = midnightReset - 18000000;
-    
-    
     var profiledata = this.state.profiledata.members[this.state.uuid];
     var forge = profiledata.forge.forge_processes.forge_1;
-    if(pearlsReset < Date.now() && profiledata.nether_island_player_data.matriarch.last_attempt < pearlsReset)
-    {
-      heavypearldisplay1 = "READY";
-      heavypearldisplay = "(" + moment(pearlsReset).fromNow() + ")";
+    if(profiledata.nether_island_player_data.matriarch.last_attempt)
+  {
+      heavypearldisplay = moment(profiledata.nether_island_player_data.matriarch.last_attempt).fromNow();
   }
-    else{
-      heavypearldisplay1 = "Collected"
-    }
     if(profiledata.active_effects.length > 28) //have a god pot
     {
       godpotdisplay = Math.round((profiledata.active_effects[0].ticks_remaining / 20 / 60 / 60) * 100) / 100;
@@ -265,20 +342,22 @@ if(forge['5']){
   forgeuntil5 =  moment(forgeend5).fromNow();
   if(forgeend5 - Date.now() > 0){
   timeleft = (forgeend5 - 180000 - Date.now())/1000; } else { timeleft = 1; }} 
-    this.savedata('i1',forgeid1)
-    this.savedata('i2',forgeid2)
-    this.savedata('i3',forgeid3)
-    this.savedata('i4',forgeid4)
-    this.savedata('i5',forgeid5)
-    this.savedata('e1', forgeend1.toString())
-    this.savedata('e2', forgeend2.toString())
-    this.savedata('e3', forgeend3.toString())
-    this.savedata('e4', forgeend4.toString())
-    this.savedata('e5', forgeend5.toString())
+
+    savedata('i1',forgeid1)
+    savedata('i2',forgeid2)
+    savedata('i3',forgeid3)
+    savedata('i4',forgeid4)
+    savedata('i5',forgeid5)
+    savedata('e1', forgeend1.toString())
+    savedata('e2', forgeend2.toString())
+    savedata('e3', forgeend3.toString())
+    savedata('e4', forgeend4.toString())
+    savedata('e5', forgeend5.toString())
+
 
       titletext = 'Dwarven Forge'
 
-      this.notify(timeleft);
+      notify(timeleft);
       } else {
         try{
         titletext = 'Dwarven Forge(Cache)'
@@ -304,43 +383,30 @@ if(forge['5']){
     
   
   return(
-<View style={{flex:1, flexDirection: 'column', backgroundColor:"#242323"}}>
-    <Text style={{paddingTop:50,fontSize:20,fontStyle:"bold",color:"cyan",fontWeight: 'bold',margin:5, textAlign:'center'}}>{titletext}</Text>
+<View style={{flex:1, flexDirection: 'column', backgroundColor:"#242323", marginTop:10}}>
+    <Text style={{paddingTop:20,fontSize:20,fontStyle:"bold",color:"cyan",fontWeight: 'bold', textAlign:'center',marginBottom:20}}>{titletext}</Text>
         <Text style={styles.text1}>{forgeid1} ending at: {forgeendtime1} ({forgeuntil1}) </Text>
         <Text style={styles.text1}>{forgeid2} ending at: {forgeendtime2} ({forgeuntil2}) </Text>
         <Text style={styles.text1}>{forgeid3} ending at: {forgeendtime3} ({forgeuntil3}) </Text>
         <Text style={styles.text1}>{forgeid4} ending at: {forgeendtime4} ({forgeuntil4}) </Text>
         <Text style={styles.text1}>{forgeid5} ending at: {forgeendtime5} ({forgeuntil5}) </Text>
+        <View style={{marginTop:10}}></View>
         <Text style={styles.text1}>God potion remaining: {godpotdisplay}h</Text>
         <Text style={styles.text1}>Heavy Pearls: {heavypearldisplay1} {heavypearldisplay}</Text>
-    <TextInput style={styles.textinput1} placeholder={"api key: " + this.state.apsc} onSubmitEditing={event =>
-    {this.getapikey(event.nativeEvent.text)}}>
-    </TextInput>
-    <TextInput style={styles.textinput1} placeholder={"username: " + this.state.uisc} onSubmitEditing={event =>
-    {this.getuuid(event.nativeEvent.text)}}>
-    </TextInput>
+    
+    <View style={{marginTop:20}}>
     <Button
-    title="Clear scheduled notifications"
+    title="Config"
     style={{textAlign:'center',backgroundColor:'gray' }}
-    onPress={async () => {
-     await Notifications.cancelAllScheduledNotificationsAsync();
-     ToastAndroid.show('Notifications cleared', ToastAndroid.SHORT)
-    }}
+    onPress={() => this.props.navigation.navigate('Config')}
     ></Button>
+    </View>
     <View style={{marginTop:20}}>
     <Button
     title="Test notification"
     style={{textAlign:'center',backgroundColor:'gray' }}
     onPress={async () => {
-      await Notifications.scheduleNotificationAsync({
-        content: {
-        title: "Forge",
-        body: 'Forge is ready',
-        priority:'max',
-        data: { data: 'collect forge' },
-        },
-        trigger: { seconds: 1 },
-        });
+      notify(1,'Forge is ready',1)
         ToastAndroid.show('Tested notification', ToastAndroid.SHORT);
     }}
     ></Button>
@@ -356,8 +422,6 @@ if(forge['5']){
     }}
     ></Button>
     </View>
-
-    
     </View>
   );
 }  
@@ -365,55 +429,97 @@ if(forge['5']){
 }
 
 
+
+class Config extends React.Component{
+  state = {
+    interval:8 //default value
+  }
+render(){
+  return(
+      <View style={{marginTop:20, backgroundColor:'#242323'}}>
+  <TextInput style={styles.textinput1} placeholder={"api key"} onSubmitEditing={event =>
+  {getapikey(event.nativeEvent.text)}}>
+  </TextInput>
+  <TextInput style={styles.textinput1} placeholder={"username"} onSubmitEditing={event =>
+  {getuuid(event.nativeEvent.text)}}>
+  </TextInput>
+  <Button
+  title="Clear scheduled notifications"
+  style={{textAlign:'center',backgroundColor:'gray' }}
+  onPress={async () => {
+   await Notifications.cancelAllScheduledNotificationsAsync();
+   ToastAndroid.show('Notifications cleared', ToastAndroid.SHORT)
+  }}
+  ></Button>
+  <View style={{marginTop:20,marginBottom:10}}>
+  <Button
+  title="Toggle auto notification"
+  style={{textAlign:'center',backgroundColor:'gray' }}
+  onPress={() => {
+    registertask(this.state.interval)
+  }}
+  ></Button>
+  </View>
+  <TextInput style={styles.textinput1} placeholder={"Custom auto notification inverval"} onSubmitEditing={event =>
+  {
+    this.setState({interval:event.nativeEvent.text})
+  }}>
+  </TextInput>
+      </View>
+  )
+}}
+
+
+
 const styles = StyleSheet.create({
  
  
-view:
-{
-  backgroundColor:'gray',
-  textAlign:'center'
-},
-text:
-{ 
-   fontSize: 18,
-   marginBottom:8,
-    color:'white',
-    textAlign:'center',
-    fontFamily: 'sans-serif',
-    
-},
-text1:
-{ 
-   fontSize: 15,
-   marginBottom:8,
-    color:'white',
-    textAlign:'center',
-    fontFamily: 'sans-serif',
-    
-},
-textinput:
-{ 
-   fontSize: 25,
-   marginTop:60,
-   marginBottom:15,
-    color:'black',
-    placeholderTextColor:'white',
-    textAlign:'center',
-    fontFamily: 'sans-serif',
-    fontStyle: 'italic',
-    
-},
-textinput1:
-{
-  marginLeft:48,
-  marginRight:48,
-  fontSize: 20,
-   marginBottom:15,
-   backgroundColor:"gray",
-    color:'white',
-    placeholderTextColor:'white',
-    textAlign:'center',
-    fontFamily: 'sans-serif',
-    fontStyle: 'italic',
-}
-});
+  view:
+  {
+    backgroundColor:'gray',
+    textAlign:'center'
+  },
+  text:
+  { 
+     fontSize: 18,
+     marginBottom:8,
+      color:'white',
+      textAlign:'center',
+      fontFamily: 'sans-serif',
+      
+  },
+  text1:
+  { 
+     fontSize: 15,
+     marginBottom:8,
+      color:'#bcc3cf',
+      textAlign:'center',
+      fontFamily: 'sans-serif',
+      
+  },
+  textinput:
+  { 
+     fontSize: 25,
+     marginTop:60,
+     marginBottom:15,
+      color:'black',
+      placeholderTextColor:'white',
+      textAlign:'center',
+      fontFamily: 'sans-serif',
+      fontStyle: 'italic',
+      
+  },
+  textinput1:
+  {
+    marginLeft:48,
+    marginRight:48,
+    fontSize: 20,
+     marginBottom:15,
+     backgroundColor:"#595959",
+      color:'white',
+      placeholderTextColor:'white',
+      textAlign:'center',
+      fontFamily: 'sans-serif',
+      fontStyle: 'italic',
+  }
+  });
